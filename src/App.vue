5e18995bd07b1c408eee679e3ee1c7c7f2dec9fb
Polegar22 <denis.casselle@gmail.com>
2018-07-11 17:07:54 +0200

    Ajout d'un fichier de style global et tentative de full screen sur la google map
    Ajout d'un fichier de style global et tentative de full screen sur la google map

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div id="app">
   <router-view name="toolbar"></router-view>
   <main>
-    <router-view></router-view>
+    <router-view class="container"></router-view>
   </main>
 </div>
 </template>
@@ -12,40 +12,6 @@ export default {
   name: 'app',
 }
 </script>
-
-<style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+<style lang="scss">
+@import './assets/css/general.scss';
 </style>
--- src/components/FindParkingSpace.vue
+++ src/components/FindParkingSpace.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <GmapMap :center="{lat:10, lng:10}" :zoom="7" map-type-id="terrain" style="width: 100%; height: 300px">
+  <GmapMap :center="{lat:10, lng:10}" :zoom="7" map-type-id="terrain" style="width: 100%; height: 100%">
   </GmapMap>
 </div>
 </template>
@@ -13,22 +13,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
 </style>
